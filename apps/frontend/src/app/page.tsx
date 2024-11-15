d381ba493635a133008c376c44511fc63e3fb356
maryson011 <maryson011@gmail.com>
2024-11-15 12:13:49 -0300

    feat: add core #1

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,8 +1,10 @@
 import Pagina from "@/components/shared/template/Pagina";
+import { Evento } from "@/core"
 
 export default function Home() {
+  const e: Partial<Evento> = {}
   return (
-    <Pagina>
+    <Pagina className="w-full">
       <div>Inicio</div>
     </Pagina>
   );
